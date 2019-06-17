d5c2f0972fe046c8009484d96e84b35b9633ad44
Abrar <abrar.ksu@gmail.com>
2019-06-17 14:18:49 +0300

    shows the winner

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ document.querySelector('.img1').setAttribute('src' , 'images/dice' + randomNumbe
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 document.querySelector('.img2').setAttribute('src' , 'images/dice' + randomNumber2 + '.png');
+
+// Show the Winner
+var displayWinner;
+if (randomNumber1 === randomNumber2){
+  displayWinner = "Draw!";
+}
+
+else if (randomNumber1 > randomNumber2) {
+  displayWinner = "Player 1 Wins! ";
+}
+
+else{
+  displayWinner = "Player 2 Wins! ";
+}
+document.querySelector('h1').innerHTML = displayWinner;
